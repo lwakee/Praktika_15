c20db4bab77947862351f27dc79bab8566eead7e
Daniil Rashchupkin <daniilrasupkin@gmail.com>
2024-12-18 20:49:20 +0300

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ![image](https://github.com/user-attachments/assets/a1433870-b2bf-4d0e-ad83-02a2d280b058)
+UML-диаграмма 2 задания
---
+++ src/Pack/Sortlist.java
@@ -0,0 +1,23 @@
+package Pack;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Sortlist {
+    public static <E extends Comparable<E>> void sort(ArrayList<E> list) {
+        Collections.sort(list);
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(5);
+        numbers.add(0);
+        numbers.add(4);
+        numbers.add(3);
+        numbers.add(9);
+
+        System.out.println(numbers);
+        sort(numbers);
+        System.out.println(numbers);
+    }
+}
+
